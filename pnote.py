b5f75966f7eecb9b867bf8456f9ab04e66829b05
allophanate <b.tenny@posteo.de>
2019-05-14 08:30:57 +0200

    started on cli, added del method

--- pnote.py
+++ pnote.py
@@ -6,11 +6,16 @@ KBASE_FILE = ""
 class KnowledgeBase():
     def __init__(self):
         tagdict = {}
+        
     def addDot(self, kdot):
         if kdot.tag in self.tagdict:
             self.tagdict[tag].append({"priority": kdot.prio, "content": kdot.content}
         else:
             self.tagdict[tag] = [{"priority": kdot.prio, "content": kdot.content}]
+            
+    def del_dot_in_tag(self, idx):
+        del self.tagdict[tag][idx]
+            
     def saveBase(self):
         with open(KBASE_FILE, "w") as outfile:
             json.dump(self.tagdict, outfile, indent=4)
@@ -18,6 +23,7 @@ class KnowledgeBase():
     def loadBase(self):
         with open(KBASE_FILE) as infile:
             self.tagdict = json.load(infile)
+            
             
     def show_in_tag(self, tag):
         for i, kdot in enumerate(self.tagdict[tag]):
@@ -30,4 +36,24 @@ class KDot():
         self.contentent = content
         self.prio = prio
         
+        
+class PyNotePrompt(cmd.Cmd):
+
+    def do_new(self, tag):
+        content = input("Enter text: ")
+        if content == "":
+        print("Please enter some text!")
+        
+        kdot = KDot(
+            
+
+
+
+    def do_greet(self, line):
+        print('hello')
+        
+    def do_EOF(self, line):
+        return True
+if __name__ == '__main__':   
+    #MyPrompt().cmdloop()
     
