658826f0764e2204fd6100293065e3c7661e1c7d
allophanate <b.tenny@posteo.de>
2019-05-15 09:30:17 +0200

    expanded cli

--- pnote.py
+++ pnote.py
@@ -7,7 +7,7 @@ class KnowledgeBase():
     def __init__(self):
         tagdict = {}
         
-    def addDot(self, kdot):
+    def add_dot(self, kdot):
         if kdot.tag in self.tagdict:
             self.tagdict[tag].append({"priority": kdot.prio, "content": kdot.content}
         else:
@@ -16,22 +16,17 @@ class KnowledgeBase():
     def del_dot_in_tag(self, idx):
         del self.tagdict[tag][idx]
             
-    def saveBase(self):
+    def save_base(self):
         with open(KBASE_FILE, "w") as outfile:
             json.dump(self.tagdict, outfile, indent=4)
     
-    def loadBase(self):
+    def load_base(self):
         with open(KBASE_FILE) as infile:
             self.tagdict = json.load(infile)
-            
-            
-    def show_in_tag(self, tag):
-        for i, kdot in enumerate(self.tagdict[tag]):
-            print("%d: %s...", i+1, kdot[content][:20])
         
         
 class KDot():
-    def __init__(self, tag="general", content="", prio=4):
+    def __init__(self, tag="general", content="", prio=5):
         self.tag = tag
         self.contentent = content
         self.prio = prio
@@ -42,15 +37,32 @@ class PyNotePrompt(cmd.Cmd):
     def do_new(self, tag):
         content = input("Enter text: ")
         if content == "":
-        print("Please enter some text!")
-        
-        kdot = KDot(
+            print("Please enter some text!")
+        else:
+            if tag == "":
+                kdot = KDot(content=content)
+            else:
+                kdot = KDot(tag=tag, content=content)
+            kbase.add_dot(kdot)
             
-
-
-
-    def do_greet(self, line):
-        print('hello')
+    def do_load(self):
+        kbase.load_base()
+        
+    def do_commit(self):
+        kbase.save_base()
+        
+    
+class XplorePrompt(cmd.Cmd):
+        
+    def do_tag(self, tag):
+        if tag == "":
+            print("List of tags:")
+            for key in kbase.tagdict.keys():
+                print(key)
+                
+        else:
+            for i, dot in enumerate(kbase.tagdict[tag]):
+                print("%d: %s..." % (i, dot[:20]))
         
     def do_EOF(self, line):
         return True
